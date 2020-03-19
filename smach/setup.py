8ac26168d7b434f968fc12b920ef87471f310abf
LouisChen1905 <cszzys@gmail.com>
2020-03-19 10:41:17 +0800

    building passed

--- smach/setup.py
+++ smach/setup.py
@@ -1,11 +1,37 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import find_packages
+from setuptools import setup
 
-d = generate_distutils_setup(
-   packages=['smach'],
-   package_dir={'': 'src'}
-)
+package_name = 'smach'
 
-setup(**d)
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages('src'),
+#    packages=find_namespace_packages(where="src"),
+    py_modules=[
+      'smach.concurrence',
+      'smach.container',
+      'smach.exceptions',
+      'smach.iterator',
+      'smach.log',
+      'smach.sequence',
+      'smach.state',
+      'smach.state_machine',
+      'smach.user_data',
+      'smach.util'
+      ],
+    package_dir={"": "src"},
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+        ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Louis Chen',
+    maintainer_email='cszzys@gmail.com',
+    description='TODO: Package description',
+    license='Apache License, Version 2.0',
+    test_suite='test',
+    tests_require=['pytest'],
+    )
--- smach_ros/setup.py
+++ smach_ros/setup.py
@@ -1,11 +1,32 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import setup
+from setuptools import find_packages
 
-d = generate_distutils_setup(
-   packages=['smach_ros'],
-   package_dir={'': 'src'}
+package_name = 'smach_ros'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages('src', exclude=['test']),
+    py_modules=[
+      'smach_ros.action_server_wrapper',
+      'smach_ros.condition',
+      'smach_ros.introspection',
+      'smach_ros.monitor_state',
+      'smach_ros.service_state',
+      'smach_ros.simple_action_state',
+      'smach_ros.util'
+      ],
+    package_dir={"": "src"},
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Louis Chen',
+    maintainer_email='cszzys@gmail.com',
+    license='Apache License, Version 2.0',
+    test_suite='test',
+    tests_require=['pytest'],
 )
-
-setup(**d)
